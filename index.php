6c7639c3f94e6fa05259ed4a4283be4ad582adbc
RootViolence <guilhermemendes.trabalho@gmail.com>
2024-06-25 02:43:56 -0300

    Adicionando pagina do jogo CURSED, JAVASCRIP para contato de confirmaçao do envio do email, estilizacao, botoes extras em algumas partes.

--- index.php
+++ index.php
@@ -4,40 +4,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TeachPlay</title>
+    <title>SK GAMES</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="icon shortcut" href="">
+    <link rel="icon shortcut" href="imagens/logos/TachPlayINVETIDO.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
-    <header class="header">
-        <nav class="nav">
+    <header class="header mx-auto">
+        <nav class="nav ">
 
             <a class="logo" href="home">SK GAMES</a>
 
             <ul class="nav-list">
 
                 <li><a href="equipe">EQUIPE</a></li>
-                <li><a href="">GAMES</a></li>
+                <li><a href="home">GAMES</a></li>
                 <li><a href="contato">CONTATO</a></li>
             </ul>
         </nav>
     </header>
     <main>
         <?php
-
         if (isset($_GET["param"])) {
-
             $param = $_GET["param"];
-
-            //separar o parametro por "/"
             $p = explode("/", $param);
-
-            //print_r($p);
-
         }
 
         $page = $p[0] ?? "home";
@@ -46,22 +45,22 @@
         if ($page == "jogo") {
             $pagina = "jogo/{$jogo}.php";
         } else {
-            $pagina = "paginas/{$page}.php}";
+            $pagina = "paginas/{$page}.php";
         }
 
-        //verificcar se a pagina exite
         if (file_exists($pagina)) {
             include $pagina;
         } else {
             include "paginas/404.php";
         }
         ?>
-    </main>
-    
-</body>
-<footer>
 
-    <p>Desenvolvido Por : <a href="" target="_blank"></a></p>
+    </main>
+</body>
+<footer class="text-center container-fluid">
+
+    <p class="text-white">Desenvolvido Por : <a href="https://github.com/RootViolence" target="_blank">Guilherme
+            Mendes</a></p>
 
 </footer>
 
---
+++ paginas/cursed.php
@@ -0,0 +1,76 @@
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+  integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+
+
+<!-- --------------------------------------------------------------- -->
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-lg-7">
+            <div id="carouselExample" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="imagens/pagina/curse/carrosel1.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="imagens/pagina/curse/carrosel2.jpg" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev bg-dark" type="button" data-target="#carouselExample" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next bg-dark" type="button" data-target="#carouselExample" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+            <div class="container mt-5">
+    <p class="" style="color:#BEBEBE;">
+        SOBRE ESTE JOGO<br>
+        Entre no mundo perigoso e cheio de ação de Rua do Samurai, um jogo de combate intenso onde você assume o papel de um samurai solitário, determinado a livrar sua cidade da ameaça de bandidos implacáveis. Com sua espada afiada e habilidades de luta excepcionais, você enfrentará ondas de inimigos em batalhas de rua eletrizantes. Defenda os inocentes, restabeleça a ordem e prove seu valor como o verdadeiro guardião da justiça neste jogo de ação imersivo e dinâmico. Prepare-se para duelos épicos e combates corpo a corpo que testarão sua habilidade e honra de samurai.
+    </p>
+</div>
+
+        </div>
+        <div class="col-lg-5">
+            <div class="container mt-3 borda rounded p-3">
+                <img src="imagens/pagina/curse/banner01.jpg" class="img-fluid mb-3" alt="Banner 01">
+                <div class="text">
+                    <p class="fs-5" style="color:#BEBEBE;">
+                        Entre no mundo perigoso e cheio de ação de Rua do Samurai, um jogo de combate intenso onde você assume o papel de um samurai solitário, determinado a livrar sua cidade da ameaça de bandidos implacáveis.
+                    </p>
+                    <p class="texto-sobre fs-5" style="color:#BEBEBE;">
+                        TODAS AS ANÁLISES:<br>
+                        Muito Positivas<br>
+                        DATA DE LANÇAMENTO:<br>
+                        20/mai./2024<br>
+                        DESENVOLVEDOR:<br>
+                        Guilherme Mendes
+                    </p>
+                </div>
+                <a href="contato" class="btn btn-primary btn-block bg-purple mt-3">Jogar</a>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+
+
+<div class="requisitos">
+
+
+
+
+
+
+
+
+</div>
